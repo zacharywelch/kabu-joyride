06b1a55b358debf911cf62ec257d83742ba466f8
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-05-12 21:41:18 -0400

    streamline css and js

--- kabu-joyride.js
+++ kabu-joyride.js
@@ -40,11 +40,11 @@
       'preStepCallback'      : $.noop,    // A method to call before each step
       'postStepCallback'     : $.noop,    // A method to call after each step
       'template' : { // HTML segments for tip layout
-        'link'    : '<a href="#close" class="joyride-close-tip">X</a>',
+        'link'    : '<a href="#close" class="joyride-close-tip quiet pin-right close"><i class="icon icon-remove"></i></a>',
         'timer'   : '<div class="joyride-timer-indicator-wrap"><span class="joyride-timer-indicator"></span></div>',
-        'tip'     : '<div class="joyride-tip-guide"><span class="joyride-nub"></span></div>',
+        'tip'     : '<div class="joyride-tip-guide pin-topleft fill-denim dark pad2 lift round"><span class="joyride-nub"></span></div>',
         'wrapper' : '<div class="joyride-content-wrapper" role="dialog"></div>',
-        'button'  : '<a href="#" class="joyride-next-tip"></a>',
+        'button'  : '<a href="#" class="joyride-next-tip btn btn-primary"></a>',
         'modal'   : '<div class="joyride-modal-bg"></div>',
         'expose'  : '<div class="joyride-expose-wrapper"></div>',
         'exposeCover': '<div class="joyride-expose-cover"></div>'
